4bbe7e3c44fbf32b4f5498cfceac3c41b5dd205f
Dylan Decrulle <dylan.decrulle@insee.fr>
2021-04-16 16:29:28 +0200

    instal pwa

--- ionic-app/src/pages/Tab1.tsx
+++ ionic-app/src/pages/Tab1.tsx
@@ -1,13 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import React from 'react';
+import { InstallPWA } from '../instalPWA';
 
 const Tab1: React.FC = () => {
+
+
   return (
     <IonPage>
       <IonHeader >
         <IonToolbar>
-          <IonTitle>Application Minimale</IonTitle>
+          <IonTitle> Application Minimale </IonTitle>
+          <IonButtons slot="primary">
+            <InstallPWA />
+          </IonButtons>
+
+
+
+
         </IonToolbar>
       </IonHeader>
       <IonContent >
