175ecb4ae3180d0adc933e09ff431a4ada0fe911
Dylan Decrulle <dylan.decrulle@insee.fr>
2021-06-28 10:32:25 +0200

    Survey Units Card

--- src/components/common/survey-unit-card/component.js
+++ src/components/common/survey-unit-card/component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Section from './section';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Phone from './phone';
@@ -8,6 +7,13 @@ import OtherContact from './other-contact';
 import ListAction from './list-action';
 import Mail from './mail';
 import Note from './note';
+import { useParams } from 'react-router-dom';
+
+import PlaceIcon from '@material-ui/icons/Place';
+import PhoneIcon from '@material-ui/icons/Phone';
+import MessageIcon from '@material-ui/icons/Message';
+import MailIcon from '@material-ui/icons/Mail';
+import EventIcon from '@material-ui/icons/Event';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -16,33 +22,55 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import { Typography } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles((theme) => ({
 	root: {},
-	appBar: {
-
-	},
+	appBar: {},
 	title: {
-    flexGrow: 1,
-  },
-	buttonTopRight:{
+		flexGrow: 1,
+	},
+	buttonTopRight: {
 		marginRight: theme.spacing(2),
-	}
+	},
+	icons: {
+		textAlign: 'center',
+		paddingTop: '0.3em'
+	},
 }));
 
-const SurveyUnitCard = ({ surveyUnits }) => {
+const SurveyUnitCard = () => {
 	const classes = useStyles();
 	const isFavorite = true;
+	const { id } = useParams();
+
 
 	return (
 		<div className={classes.root}>
-			<AppBar position="static" color="default">
+			<AppBar position="static" color="transparent">
 				<Toolbar>
-					<ButtonIcon color='inherit' className={classes.buttonTopRight} icon={<NavigateBeforeIcon />} />
+					<ButtonIcon
+						color="inherit"
+						className={classes.buttonTopRight}
+						icon={<NavigateBeforeIcon />}
+					/>
 					<Typography className={classes.title}>Nom Prénom</Typography>
-					<ButtonIcon color='inherit' icon={isFavorite ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />} />
+					<ButtonIcon
+						color="inherit"
+						icon={
+							isFavorite ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />
+						}
+					/>
 				</Toolbar>
 			</AppBar>
+			<div className={classes.icons}>
+				<ButtonIcon icon={<PhoneIcon />} color='inherit'/>
+				<ButtonIcon icon={<MessageIcon />} color='inherit'/>
+				<ButtonIcon icon={<MailIcon />} color='inherit'/>
+				<ButtonIcon icon={<EventIcon />} color='inherit'/>
+				<ButtonIcon icon={<PlaceIcon />} color='inherit'/>
+				<Divider />
+			</div>
 
 			<Phone />
 			<Place />
@@ -55,14 +83,9 @@ const SurveyUnitCard = ({ surveyUnits }) => {
 };
 
 SurveyUnitCard.propTypes = {
-	/**
-	 * surveyUnits is the json data
-	 */
-	surveyUnits: PropTypes.element,
 };
 
 SurveyUnitCard.defaultProps = {
-	surveyUnits: {},
 };
 
 export default SurveyUnitCard;
--- src/components/common/survey-unit-card/survey-unit-card.stories.js
+++ src/components/common/survey-unit-card/survey-unit-card.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import SurveyUnitCard from './component';
+import { MemoryRouter, Route} from "react-router-dom"
 
 export default {
 	title: 'Components/SurveyUnitCard',
 	component: SurveyUnitCard,
+	decorators: [(Story) => (
+		<MemoryRouter initialEntries={["/11"]}>
+				<Route path="/:id">
+						<SurveyUnitCard />
+				</Route>
+		</MemoryRouter>)],
 };
 
 const Template = (args) => <SurveyUnitCard {...args} />;
